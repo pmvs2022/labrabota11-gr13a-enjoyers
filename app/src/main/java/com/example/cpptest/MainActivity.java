fe634e0f73a4e5125cf461a7356b8dcfce1eae2a
Maxim Andrievskiy <maxim.andrievskiy2003@gmail.com>
2023-01-10 12:32:44 +0300

    update main activity

--- app/src/main/java/com/example/cpptest/MainActivity.java
+++ app/src/main/java/com/example/cpptest/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.cpptest.databinding.ActivityMainBinding;
@@ -21,9 +22,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     static {
         System.loadLibrary("cpptest");
     }
-
+    private EditText etUsername;
+    private EditText etPassword;
     private ActivityMainBinding binding;
     private Context context;
+    private String username;
+    private String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.Theme_CppTest);
@@ -32,8 +36,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         context = getApplicationContext();
         ConstraintLayout root = binding.getRoot();
+        etUsername = (EditText) root.findViewById(R.id.username);
+        etPassword = (EditText) root.findViewById(R.id.password);
         setContentView(root);
-        setButtonSettings();
+        setButtonsSettings();
         Intent myIntent = new Intent(MainActivity.this, OptionsActivity.class);
         MainActivity.this.startActivity(myIntent);
     }
@@ -48,21 +54,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        switch (((View)view.getParent()).getId()){
-            case R.id.login_button:
-                Toast.makeText( context, "This is login" + "\n" + R.id.login_button,Toast.LENGTH_LONG).show();
-                boolean registerConfirmed = true;
-                if(registerConfirmed){
-                    Intent myIntent = new Intent(MainActivity.this, OptionsActivity.class);
-                    myIntent.putExtra("key", "bruh"); //Optional parameters
-                    MainActivity.this.startActivity(myIntent);
-                }
-                break;
+        try {
+            username = etUsername.getText().toString();
+            password = etPassword.getText().toString();
+
+            switch (((View) view.getParent()).getId()) {
+                case R.id.login_button:
+                    if (authorizeUser(0, username, password)) {
+                        Intent myIntent = new Intent(MainActivity.this, OptionsActivity.class);
+                        Toast.makeText(context, "Logged in successfully!", Toast.LENGTH_LONG).show();
+                        MainActivity.this.startActivity(myIntent);
+                    } else {
+                        Toast.makeText(context, "Wrong Credentials\nInvalid username or password", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                case R.id.signup_button:
+                    if (authorizeUser(1, username, password)) {
+                        Intent myIntent = new Intent(MainActivity.this, OptionsActivity.class);
+                        Toast.makeText(context, "Signed up successfully!", Toast.LENGTH_LONG).show();
+                        MainActivity.this.startActivity(myIntent);
+                    } else {
+                        Toast.makeText(context, "Wrong Credentials\nInvalid username (must match email format) or password (must contain at least 8 characters, at least one letter and one digit)", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.toString());
         }
     }
 
-    private void setButtonSettings() {
+    private void setButtonsSettings() {
         findViewById(R.id.login_button).findViewById(R.id.button).setOnClickListener(this);
-        ((AppCompatButton)findViewById(R.id.login_button).findViewById(R.id.button)).setText("Log in/Register");
+        findViewById(R.id.signup_button).findViewById(R.id.button).setOnClickListener(this);
+        ((AppCompatButton)findViewById(R.id.login_button).findViewById(R.id.button)).setText("Log in");
+        ((AppCompatButton)findViewById(R.id.signup_button).findViewById(R.id.button)).setText("Sign up");
     }
 }
