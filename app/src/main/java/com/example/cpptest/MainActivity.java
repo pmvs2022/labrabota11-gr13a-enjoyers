bea91f2cd008ed5b139c42724e14468ae2c0f754
RifeX-LA <timur322132@mail.ru>
2023-01-12 00:52:15 +0300

    fix db access and permissions

--- app/src/main/java/com/example/cpptest/MainActivity.java
+++ app/src/main/java/com/example/cpptest/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.cpptest;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -45,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setButtonsSettings();
         Intent myIntent = new Intent(MainActivity.this, OptionsActivity.class);
         MainActivity.this.startActivity(myIntent);
+        requestStoragePermissions();
     }
 
 
@@ -94,5 +99,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButtonFragment.addTouchDelegate(findViewById(R.id.signup_button).findViewById(R.id.button),this);
         ((AppCompatButton) findViewById(R.id.login_button).findViewById(R.id.button)).setText("Log in");
         ((AppCompatButton) findViewById(R.id.signup_button).findViewById(R.id.button)).setText("Sign up");
+    }
+
+    private void requestStoragePermissions() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
     }
 }
